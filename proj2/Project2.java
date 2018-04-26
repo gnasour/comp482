64f31cf7454b192e7aa3d41b2c1b92f73ce3a145
George Nassour <gnasour@gmail.com>
2018-04-25 23:47:22 -0700

    Finished project 2, add comments

--- proj2/Project2.java
+++ proj2/Project2.java
@@ -27,45 +27,40 @@ class Project2{
         j = subsequence_string_2.length();
         k = subsequence_string_3.length();
         //Declare 3D array size
-        three_string_array = new int [i][j][k];
+        three_string_array = new int [i+1][j+1][k+1];
+        fill_array_with_zeroes();
         least_Common_Substring();
-        System.out.print(three_string_array[i-1][j-1][k-1]);
+        System.out.print(three_string_array[i][j][k]);
 
-        //System.out.println(l);
     }
     
-    //The recursive method that will populate our three string array
-     /*public static int least_Common_Substring(int [][][] three_string_array, int tempI, int tempJ, int tempK){
-        i = tempI;
-        j = tempJ;
-        k = tempK;
-        if(i == 0 || j == 0 || k == 0){
-            return 0;
-        }else if(subsequence_string_1.charAt(i-1) == subsequence_string_2.charAt(j-1) && 
-            subsequence_string_2.charAt(j-1) == subsequence_string_3.charAt(k-1)){
-                i-=1;
-                j-=1;
-                k-=1;
-                return 1 + least_Common_Substring(three_string_array,i,j,k);
-        }else { //If one of the characters are not equal then check the max of all the subtractions
-                return Math.max(
-                    Math.max(least_Common_Substring(three_string_array,i-1,j,k),least_Common_Substring(three_string_array,i,j-1,k))
-                    , least_Common_Substring(three_string_array,i,j,k-1));
-            }
-
-    }*/
+    
 
     public static void least_Common_Substring(){
-        for(int m = 0; m < i; m++){
-            for(int n = 0; n < j; n++){
-                for(int o = 0; o < k; o++){
-                    if(m == 0 || n == 0 || o == 0 )
-                        three_string_array[m][n][o] = 0;
-                    else if(subsequence_string_1.charAt(m-1) == subsequence_string_2.charAt(n-1) &&
+        for(int m = 1; m < i+1; m++){
+            for(int n = 1; n < j+1; n++){
+                for(int o = 1; o < k+1; o++){
+                    if(subsequence_string_1.charAt(m-1) == subsequence_string_2.charAt(n-1) &&
                             subsequence_string_2.charAt(n-1) == subsequence_string_3.charAt(o-1)){
                         three_string_array[m][n][o] = 1 + three_string_array[m-1][n-1][o-1];
                     }else{
                         three_string_array[m][n][o] = Math.max(Math.max(three_string_array[m-1][n][o], three_string_array[m][n-1][o]),three_string_array[m][n][o-1]);
+                    }
+                }
+            }
+        }
+
+    }
+    /**
+     * Fill the array with zeroes for the base case
+     * BASE CASE: If any of the strings are lengths of 0, then
+     */
+    public static void fill_array_with_zeroes(){
+        for(int m = 0; m < i; m++){
+            for(int n = 0; n < j; n++){
+                for(int o = 0; o < k; o++){
+                    if(m == 0 || n == 0 || o == 0 ) {
+                        three_string_array[m][n][o] = 0;
                     }
                 }
             }
