480ebd33ec15ea357bca31d9bc680ab975d4ad8c
George Nassour <gnasour@gmail.com>
2018-05-14 11:54:53 -0700

    Commented some variables and functions

--- proj3/Project3.java
+++ proj3/Project3.java
@@ -13,58 +13,79 @@ import java.util.Random;
 class Project3{
 
     private static Scanner scanner;
+    //These two arraylists will act as a column on the input
+    //One and Two corrsepond to the relative columns
     private static ArrayList<Integer> satArrayListOne = new ArrayList<Integer>();
     private static ArrayList<Integer> satArrayListTwo = new ArrayList<Integer>();
+    //This is the largest variable in the file after applying Math.abs to it
     private static int largestVariable = 0;
+    //After reaching the most significant bit, reduce this variable by one to not cause overflow
     private static int maxVariation = 0;
+    //Do all variables false first before changing the boolean array
     private static boolean triggerAllFalseResult = false;
+    //Boolean array that holds the variations for the truth values of the variables
     private static boolean booleanArray[];
+    //The boolean array that had the most satisified truth assignments
     private static boolean bestBooleanArray[];
+    //Used to calculate the number of times a variable appears in the text file
     private static int probabilityArray[][];
-    //The most lines we have satisfied
+    //The most disjunctions we have satisfied
     private static int mostVariables = 0;
-    private static int calculateSubsets = 0;
+    //How many subsets we have to do
+    //Brute forced sets
+    private static long calculateSubsets = 0;
+    //Binary addition on boolean array
+    //If we changed a position to true(1) then do not change anymore booleans(bits)
     private static boolean changedToTrue = false;
+    //The truth assignments of the boolean array in string format
+    private static String truthValueString = "";
+
     /**
      Main method to start off our application.
      Tasks:
      1.) Scans input 3 text for the inputs
      2.) Ask user which algorithm to use
-     3.) Use the algorithm chosen
+     3.) Use the algorithm chosen to compute the result
      */
     public static void main(String [] args){
-        int choice, answer;
+        //Used to store what the user's choice in method to compute
+        int choice;
+
         //Read the file into scanner
         try{
-            scanner = new Scanner(new File("src/input3.txt"));
+            scanner = new Scanner(new File("input3.txt"));
         }catch(FileNotFoundException fnf){
             System.out.println("The file you have specified was not found: \n" + fnf.toString());
         }
+
         //Read the numbers into two arraylists
         //The ArrayLists one and two correspond to the columns of the variables in the text file
-        while(scanner.hasNextLine()){
+        while(scanner.hasNextInt()){
             satArrayListOne.add(scanner.nextInt());
             satArrayListTwo.add(scanner.nextInt());
         }
+
         //Find the largest variable
         for(int i = 0; i < satArrayListTwo.size(); i++){
             if(Math.abs((int)satArrayListOne.get(i)) > largestVariable){
                 largestVariable = Math.abs((int)satArrayListOne.get(i));
-            }else if(Math.abs((int)satArrayListTwo.get(i)) > largestVariable){
+            }
+            if(Math.abs((int)satArrayListTwo.get(i)) > largestVariable){
                 largestVariable = Math.abs((int)satArrayListTwo.get(i));
             }
         }
+
         //boolean array for brute forcing
         booleanArray = new boolean[largestVariable];
         //Used to calculate the recurrence of certain variables
         probabilityArray = new int[largestVariable][2];
         //Calculate the max times around the for loop for brute force
-        calculateSubsets = (int)Math.pow(2, largestVariable);
+        calculateSubsets = (long)Math.pow(2, largestVariable);
         //Use this variable to keep track of most significant bit
         maxVariation = largestVariable;
 
 
-
+        //User input for algorithm
         System.out.print("Enter 1 for Brute Force or 2 for Best Polynomial Algorithm: ");
         scanner = new Scanner(System.in);
         choice = scanner.nextInt();
@@ -73,16 +94,18 @@ class Project3{
         }else if(choice == 2){
             use_best_poly_algo();
         }else
-            System.out.print("Wrong input");
+            System.out.print("Please enter only 1 or 2 when you run the program next time");
 
     }
 
     /**
      * Uses brute force to find the solution of the SAT problem
+     * This algorithm computes all subsets of the problem to find the optimal solution
+     * 2^n subsets + n comparisons
      */
     public static void use_brute_force() {
 
-        for (int i = 0; i < calculateSubsets; i++) {
+        for (long i = 0; i < calculateSubsets; i++) {
             //Use the initial all-false array
             //Then start doing the bit addition to change the boolean values
             if(triggerAllFalseResult)
@@ -103,18 +126,29 @@ class Project3{
                 }
             }
             if(localMostVariable > mostVariables){
-                mostVariables =localMostVariable;
+                mostVariables = localMostVariable;
                 bestBooleanArray = booleanArray.clone();
             }
 
         }
-        for(int i = 0; i < booleanArray.length; i++)
-            System.out.print(bestBooleanArray[i]+" ");
-        System.out.println(mostVariables);
+        System.out.print(mostVariables + " ");
+        for(int i = 0; i < bestBooleanArray.length; i++)
+            if(bestBooleanArray[i]){
+                truthValueString += "T";
+            }else{
+                truthValueString += "F";
+            }
+        System.out.print(truthValueString);
 
     }
+
     /**
      * Using a statistical method to find the solution of the SAT problem
+     * 2n to compute the ratio of variables to satisfy
+     * n to compute the boolean array
+     * n to compute the correct amount of disjunctions
+     * n to write result to string
+     * O(n) time complexity
      */
     public static void use_best_poly_algo(){
         calculateProbability();
@@ -145,12 +179,19 @@ class Project3{
                 mostVariables += 1;
             }
         }
-        for(boolean s : booleanArray){
-            System.out.println(s);
-        }
-        System.out.print(mostVariables);
+        System.out.print(mostVariables + " ");
+        for(int i = 0; i < booleanArray.length; i++)
+            if(booleanArray[i]){
+                truthValueString += "T";
+            }else{
+                truthValueString += "F";
+            }
+        System.out.print(truthValueString);
     }
-
+    /**
+     * Calculates the amount of variables and checks how many are inverted or not
+     * One for loop for each arraylist
+     */
     public static void calculateProbability(){
         for(int variableNumber:satArrayListOne){
             if(variableNumber > 0)
@@ -166,6 +207,9 @@ class Project3{
         }
     }
 
+    /**
+     * Use a boolean array and compute binary addition on it until you have exhausted all truth assignment possibilities
+     */
     public static void changeArray(){
         for (int i = 0; i <= maxVariation; i++){
             if ( i != maxVariation) {
