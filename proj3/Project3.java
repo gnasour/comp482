6d7c807b277eb7e4dcb865e0ce210e415b4ede0e
George Nassour <gnasour@gmail.com>
2018-05-13 18:49:35 -0700

    Implemented probability polynomial solution

--- proj3/Project3.java
+++ proj3/Project3.java
@@ -8,17 +8,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 
 class Project3{
 
     private static Scanner scanner;
-    private static ArrayList satArrayListOne = new ArrayList();
-    private static ArrayList satArrayListTwo = new ArrayList();
+    private static ArrayList<Integer> satArrayListOne = new ArrayList<Integer>();
+    private static ArrayList<Integer> satArrayListTwo = new ArrayList<Integer>();
     private static int largestVariable = 0;
     private static int maxVariation = 0;
     private static boolean triggerAllFalseResult = false;
     private static boolean booleanArray[];
     private static boolean bestBooleanArray[];
+    private static int probabilityArray[][];
     //The most lines we have satisfied
     private static int mostVariables = 0;
     private static int calculateSubsets = 0;
@@ -46,22 +48,25 @@ class Project3{
         }
         //Find the largest variable
         for(int i = 0; i < satArrayListTwo.size(); i++){
-            if(Math.abs((int)satArrayListTwo.get(i)) > largestVariable){
-                largestVariable = Math.abs((int)satArrayListTwo.get(i));
-            }else if(Math.abs((int)satArrayListOne.get(i)) > largestVariable){
+            if(Math.abs((int)satArrayListOne.get(i)) > largestVariable){
                 largestVariable = Math.abs((int)satArrayListOne.get(i));
+            }else if(Math.abs((int)satArrayListTwo.get(i)) > largestVariable){
+                largestVariable = Math.abs((int)satArrayListTwo.get(i));
             }
         }
         //boolean array for brute forcing
         booleanArray = new boolean[largestVariable];
+        //Used to calculate the recurrence of certain variables
+        probabilityArray = new int[largestVariable][2];
         //Calculate the max times around the for loop for brute force
         calculateSubsets = (int)Math.pow(2, largestVariable);
         //Use this variable to keep track of most significant bit
         maxVariation = largestVariable;
-        use_brute_force();
+        use_best_poly_algo();
+        /*use_brute_force();
         for(int i = 0; i < booleanArray.length; i++)
             System.out.print(bestBooleanArray[i]+" ");
-        System.out.println(mostVariables);
+        System.out.println(mostVariables);*/
 
 
         /*System.out.println("Enter 1 for Brute Force or 2 for Best Polynomial Algorithm");
@@ -73,7 +78,6 @@ class Project3{
 
     /**
      * Uses brute force to find the solution of the SAT problem
-     * @return
      */
     public static void use_brute_force() {
 
@@ -107,11 +111,55 @@ class Project3{
     }
     /**
      * Using a statistical method to find the solution of the SAT problem
-     * @return
      */
-    public static int use_best_poly_algo(){
-        //Change the return statement
-        return 1;
+    public static void use_best_poly_algo(){
+        calculateProbability();
+        for(int i = 0; i < booleanArray.length; i++){
+            if(probabilityArray[i][0] > probabilityArray[i][1]){
+                booleanArray[i] = true;
+            }else if(probabilityArray[i][0] == probabilityArray[i][1]){
+                Random random = new Random();
+                int coinFlip = random.nextInt(2);
+                if(coinFlip == 0){
+                    booleanArray[i] = true;
+                }else{
+                    booleanArray[i] = false;
+                }
+
+            }else{
+                booleanArray[i] = false;
+            }
+        }
+        for (int j = 0; j < satArrayListOne.size(); j++) {
+            int truthValueOne = (int) satArrayListOne.get(j);
+            int truthValueTwo = (int) satArrayListTwo.get(j);
+            boolean truthAssignmentOne = booleanArray[Math.abs(truthValueOne)-1];
+            boolean truthAssignmentTwo = booleanArray[Math.abs(truthValueTwo)-1];
+            if((truthAssignmentOne == false && truthValueOne < 0) || (truthAssignmentOne == true && truthValueOne > 0)){
+                mostVariables += 1;
+            }else if((truthAssignmentTwo == false && truthValueTwo < 0) || (truthAssignmentTwo == true && truthValueTwo > 0)){
+                mostVariables += 1;
+            }
+        }
+        for(boolean s : booleanArray){
+            System.out.println(s);
+        }
+        System.out.print(mostVariables);
+    }
+
+    public static void calculateProbability(){
+        for(int variableNumber:satArrayListOne){
+            if(variableNumber > 0)
+                probabilityArray[Math.abs(variableNumber)-1][0] += 1;
+            else
+                probabilityArray[Math.abs(variableNumber)-1][1] += 1;
+        }
+        for(int variableNumber:satArrayListTwo){
+            if(variableNumber > 0)
+                probabilityArray[Math.abs(variableNumber)-1][0] += 1;
+            else
+                probabilityArray[Math.abs(variableNumber)-1][1] += 1;
+        }
     }
 
     public static void changeArray(){
